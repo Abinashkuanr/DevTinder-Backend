f9617cbd0d56d97d32483dffff758c62457e0308
Abinash Kuanr <abinashkuanr677@gmail.com>
2026-03-02 22:16:27 +0530

    Exploring validator lib

--- src/Models/user.js
+++ src/Models/user.js
@@ -1,5 +1,6 @@
 
 const mongoose = require('mongoose');
+const validator = require("validator")
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -18,6 +19,11 @@ const userSchema = new mongoose.Schema({
         unique: true,
         lowercase: true,
         trim: true,
+        validate(value) {
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid email address: "+ value);
+            }
+        },
 
     },
     password: {
